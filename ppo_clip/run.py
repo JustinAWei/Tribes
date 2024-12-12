2a4f0394924dfb30e18c722c04205cf26d44ad76
Justin Wei <wei.justin.a@gmail.com>
2024-12-12 13:59:22 -0800

    util file

--- ppo_clip/run.py
+++ ppo_clip/run.py
@@ -8,6 +8,8 @@ from pprint import pprint
 from datetime import datetime
 import torch
 
+from utils import ACTION_CATEGORIES, ACTION_TYPES, TECH_TYPES, BUILDING_TYPES, UNIT_TYPES, MASK, BOARD_LEN
+
 # Create FastAPI app
 app = FastAPI()
 
@@ -34,100 +36,6 @@ def create_multidimensional_mask(coordinates, shape):
 
     return mask
 
-# Reversed dictionary for action types
-ACTION_TYPES = {
-    "BUILD": 0,
-    "LEVEL_UP": 5,
-    "RESOURCE_GATHERING": 6,
-    "SPAWN": 7,
-
-    "END_TURN": 9,
-    "RESEARCH_TECH": 10,
-
-    "ATTACK": 13,
-    "CAPTURE": 14,
-    "MOVE": 20
-}
-
-# Reversed dictionary for action categories
-ACTION_CATEGORIES = {
-    "TRIBE": 0,
-    "CITY": 1,
-    "UNIT": 2
-}
-
-
-# Unit type mapping
-UNIT_TYPES = {
-    "WARRIOR": 0,
-    "RIDER": 1, 
-    "DEFENDER": 2,
-    "SWORDMAN": 3,
-    "ARCHER": 4,
-    "CATAPULT": 5,
-    "KNIGHT": 6,
-    "MIND_BENDER": 7,
-    "BOAT": 8,
-    "SHIP": 9,
-    "BATTLESHIP": 10,
-    "SUPERUNIT": 11
-}
-
-# Building type mapping
-BUILDING_TYPES = {
-    "PORT": 0,
-    "MINE": 1,
-    "FORGE": 2,
-    "FARM": 3,
-    "WINDMILL": 4,
-    "CUSTOMS_HOUSE": 5,
-    "LUMBER_HUT": 6,
-    "SAWMILL": 7,
-    "TEMPLE": 8,
-    "WATER_TEMPLE": 9,
-    "FOREST_TEMPLE": 10,
-    "MOUNTAIN_TEMPLE": 11,
-    "ALTAR_OF_PEACE": 12,
-    "EMPERORS_TOMB": 13,
-    "EYE_OF_GOD": 14,
-    "GATE_OF_POWER": 15,
-    "GRAND_BAZAR": 16,
-    "PARK_OF_FORTUNE": 17,
-    "TOWER_OF_WISDOM": 18
-}
-# Technology type mapping
-TECH_TYPES = {
-    "CLIMBING": 0,
-    "FISHING": 1,
-    "HUNTING": 2,
-    "ORGANIZATION": 3,
-    "RIDING": 4,
-    "ARCHERY": 5,
-    "FARMING": 6,
-    "FORESTRY": 7,
-    "FREE_SPIRIT": 8,
-    "MEDITATION": 9,
-    "MINING": 10,
-    "ROADS": 11,
-    "SAILING": 12,
-    "SHIELDS": 13,
-    "WHALING": 14,
-    "AQUATISM": 15,
-    "CHIVALRY": 16,
-    "CONSTRUCTION": 17,
-    "MATHEMATICS": 18,
-    "NAVIGATION": 19,
-    "SMITHERY": 20,
-    "SPIRITUALISM": 21,
-    "TRADE": 22,
-    "PHILOSOPHY": 23
-}
-
-
-BOARD_LEN = 8
-BOARD_SIZE = BOARD_LEN ** 2
-MASK = 0
-
 def get_actor_x_y(actor_id, gs):
     # in board -> gameActors
     game_actors = gs.get('board', {}).get('gameActors', {})
@@ -139,28 +47,53 @@ def get_actor_x_y(actor_id, gs):
     x = position.get('x', 0)
     y = position.get('y', 0)
 
-    print("actor_id", actor_id)
-    print(f"x: {x} | y: {y}")
+    # print("actor_id", actor_id)
+    # print(f"x: {x} | y: {y}")
 
     return x, y
 
-class FilteredActions(BaseModel):
-    """
-    Model to structure filtered city actions
-    """
-    city_id: str
-    filtered_actions: List[Dict[str, Any]]
+def ppo_clip(game_state, valid_actions):
+    NUM_CATEGORIES = 32
+    action_space_shape = (len(ACTION_CATEGORIES), max(ACTION_TYPES.values()) + 1, BOARD_LEN, BOARD_LEN, BOARD_LEN, BOARD_LEN, NUM_CATEGORIES)
 
-# request_counter = 0
+    # output matrix in the action space
+    # rand matrix between 0MASK of action_space_shape
+    # TODO: actually implement the PPO-clip
+    action_space = torch.rand(action_space_shape)
+
+    # mask
+    coordinates = torch.tensor(valid_actions)
+    print(coordinates)
+    mask = create_multidimensional_mask(coordinates, action_space_shape)
+    print("Mask shape:", mask.shape)
+    print("Number of 1s in the mask:", torch.sum(mask))
+
+    # use the mask to filter the valid actions by
+    valid_action_space = action_space * mask
+    # count nonzero elements
+    print("Number of nonzero elements:", torch.count_nonzero(valid_action_space))
+
+    # softmax and choose the action
+    valid_action_space = torch.softmax(valid_action_space, dim=len(action_space_shape) - 1)
+
+    # Flatten tensor and get argmax
+    flat_index = torch.argmax(valid_action_space.flatten())
+    
+    # Convert flat index back to multi-dimensional indices
+    action = np.unravel_index(flat_index.item(), action_space_shape)
+    # this is a np arr of np.int64, i want to convert it to a list of ints
+    action = [int(i) for i in action]
+
+    print("Action:")
+    print(action)
+
+    return action
 
 @app.post("/receive")
 async def receive_data(request: Request):
     """
     Process and filter city actions from game state
     """
-    # global request_counter
-    # request_counter += 1
-
     # Extract request data
     data = await request.json()
 
@@ -271,45 +204,6 @@ async def receive_data(request: Request):
 
 
                 valid_actions.append([ACTION_CATEGORIES["UNIT"], ACTION_TYPES[action.get('actionType')], x1, y1, x2, y2, MASK])
-
-
-        def ppo_clip(game_state, valid_actions):
-            NUM_CATEGORIES = 32
-            action_space_shape = (len(ACTION_CATEGORIES), max(ACTION_TYPES.values()) + 1, BOARD_LEN, BOARD_LEN, BOARD_LEN, BOARD_LEN, NUM_CATEGORIES)
-
-            # output matrix in the action space
-            # rand matrix between 0MASK of action_space_shape
-            # TODO: actually implement the PPO-clip
-            action_space = torch.rand(action_space_shape)
-
-            # mask
-            coordinates = torch.tensor(valid_actions)
-            print(coordinates)
-            mask = create_multidimensional_mask(coordinates, action_space_shape)
-            print("Mask shape:", mask.shape)
-            print("Number of 1s in the mask:", torch.sum(mask))
-
-            # use the mask to filter the valid actions by
-            valid_action_space = action_space * mask
-            # count nonzero elements
-            print("Number of nonzero elements:", torch.count_nonzero(valid_action_space))
-
-            # softmax and choose the action
-            valid_action_space = torch.softmax(valid_action_space, dim=len(action_space_shape) - 1)
-
-            # Flatten tensor and get argmax
-            flat_index = torch.argmax(valid_action_space.flatten())
-            
-            # Convert flat index back to multi-dimensional indices
-            action = np.unravel_index(flat_index.item(), action_space_shape)
-            # this is a np arr of np.int64, i want to convert it to a list of ints
-            action = [int(i) for i in action]
-
-            print("Action:")
-            print(action)
-
-            return action
-
 
         action = ppo_clip(gs, valid_actions)
 
---
+++ ppo_clip/utils.py
@@ -0,0 +1,93 @@
+# Reversed dictionary for action types
+ACTION_TYPES = {
+    "BUILD": 0,
+    "LEVEL_UP": 5,
+    "RESOURCE_GATHERING": 6,
+    "SPAWN": 7,
+
+    "END_TURN": 9,
+    "RESEARCH_TECH": 10,
+
+    "ATTACK": 13,
+    "CAPTURE": 14,
+    "MOVE": 20
+}
+
+# Reversed dictionary for action categories
+ACTION_CATEGORIES = {
+    "TRIBE": 0,
+    "CITY": 1,
+    "UNIT": 2
+}
+
+
+# Unit type mapping
+UNIT_TYPES = {
+    "WARRIOR": 0,
+    "RIDER": 1, 
+    "DEFENDER": 2,
+    "SWORDMAN": 3,
+    "ARCHER": 4,
+    "CATAPULT": 5,
+    "KNIGHT": 6,
+    "MIND_BENDER": 7,
+    "BOAT": 8,
+    "SHIP": 9,
+    "BATTLESHIP": 10,
+    "SUPERUNIT": 11
+}
+
+# Building type mapping
+BUILDING_TYPES = {
+    "PORT": 0,
+    "MINE": 1,
+    "FORGE": 2,
+    "FARM": 3,
+    "WINDMILL": 4,
+    "CUSTOMS_HOUSE": 5,
+    "LUMBER_HUT": 6,
+    "SAWMILL": 7,
+    "TEMPLE": 8,
+    "WATER_TEMPLE": 9,
+    "FOREST_TEMPLE": 10,
+    "MOUNTAIN_TEMPLE": 11,
+    "ALTAR_OF_PEACE": 12,
+    "EMPERORS_TOMB": 13,
+    "EYE_OF_GOD": 14,
+    "GATE_OF_POWER": 15,
+    "GRAND_BAZAR": 16,
+    "PARK_OF_FORTUNE": 17,
+    "TOWER_OF_WISDOM": 18
+}
+# Technology type mapping
+TECH_TYPES = {
+    "CLIMBING": 0,
+    "FISHING": 1,
+    "HUNTING": 2,
+    "ORGANIZATION": 3,
+    "RIDING": 4,
+    "ARCHERY": 5,
+    "FARMING": 6,
+    "FORESTRY": 7,
+    "FREE_SPIRIT": 8,
+    "MEDITATION": 9,
+    "MINING": 10,
+    "ROADS": 11,
+    "SAILING": 12,
+    "SHIELDS": 13,
+    "WHALING": 14,
+    "AQUATISM": 15,
+    "CHIVALRY": 16,
+    "CONSTRUCTION": 17,
+    "MATHEMATICS": 18,
+    "NAVIGATION": 19,
+    "SMITHERY": 20,
+    "SPIRITUALISM": 21,
+    "TRADE": 22,
+    "PHILOSOPHY": 23
+}
+
+
+BOARD_LEN = 8
+BOARD_SIZE = BOARD_LEN ** 2
+MASK = 0
