29421a64a74f5b4fff94143016238c95ecf7d8d3
Justin Wei <wei.justin.a@gmail.com>
2024-12-12 17:27:43 -0800

    get action

--- ppo_clip/model.py
+++ ppo_clip/model.py
@@ -5,32 +5,40 @@ import numpy as np
 class PPOClipAgent:
     def __init__(self, action_space_shape):
         self.action_space_shape = action_space_shape
+        self._actor = Actor(action_space_shape)
+        self._critic = Critic(action_space_shape)
 
     def update(self, game_state, valid_actions):
         pass
 
     def get_action(self, valid_actions):
+        action_space_logits = self._actor.forward(valid_actions)
+
         print(valid_actions)
         mask = create_multidimensional_mask(torch.tensor(valid_actions), self.action_space_shape)
         print("Mask shape:", mask.shape)
 
         # use the mask to filter the valid actions by
-        valid_action_space = action_space * mask
+        valid_action_space = action_space_logits * mask
         # count nonzero elements
         print("Number of nonzero elements:", torch.count_nonzero(valid_action_space), "Number of 1s in the mask:", torch.sum(mask))
 
         # softmax and choose the action
-        valid_action_space = torch.softmax(valid_action_space, dim=len(action_space_shape) - 1)
+        valid_action_space = torch.softmax(valid_action_space, dim=len(self.action_space_shape) - 1)
 
         # Flatten tensor and get argmax
         flat_index = torch.argmax(valid_action_space.flatten())
         
         # Convert flat index back to multi-dimensional indices
-        action = np.unravel_index(flat_index.item(), action_space_shape)
+        action = np.unravel_index(flat_index.item(), self.action_space_shape)
         # this is a np arr of np.int64, i want to convert it to a list of ints
         action = [int(i) for i in action]
 
         print("Action: ", action)
+        return action
+
+    def gae(self, rewards, values, dones):
+        pass
 
 class Actor(nn.Module):
     def __init__(self, action_space_shape):
@@ -72,32 +80,7 @@ def create_multidimensional_mask(coordinates, shape):
     return mask
 
 def ppo_clip(game_state, valid_actions, action_space_shape):
-
-    # output matrix in the action space
-    # rand matrix between 0MASK of action_space_shape
-    # TODO: actually implement the PPO-clip
-    action_space = torch.rand(action_space_shape)
-
-    print(valid_actions)
-    mask = create_multidimensional_mask(torch.tensor(valid_actions), action_space_shape)
-    print("Mask shape:", mask.shape)
-
-    # use the mask to filter the valid actions by
-    valid_action_space = action_space * mask
-    # count nonzero elements
-    print("Number of nonzero elements:", torch.count_nonzero(valid_action_space), "Number of 1s in the mask:", torch.sum(mask))
-
-    # softmax and choose the action
-    valid_action_space = torch.softmax(valid_action_space, dim=len(action_space_shape) - 1)
-
-    # Flatten tensor and get argmax
-    flat_index = torch.argmax(valid_action_space.flatten())
-    
-    # Convert flat index back to multi-dimensional indices
-    action = np.unravel_index(flat_index.item(), action_space_shape)
-    # this is a np arr of np.int64, i want to convert it to a list of ints
-    action = [int(i) for i in action]
-
+    agent = PPOClipAgent(action_space_shape)
+    action = agent.get_action(valid_actions)
     print("Action: ", action)
-
     return action
