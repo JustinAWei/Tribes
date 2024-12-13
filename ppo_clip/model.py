5d7e74927731e3356d3742bafd933dfb7520a586
Justin Wei <wei.justin.a@gmail.com>
2024-12-12 17:25:45 -0800

    scaffold for ac

--- ppo_clip/model.py
+++ ppo_clip/model.py
@@ -1,8 +1,55 @@
 import torch
 import numpy as np
 
+
+class PPOClipAgent:
+    def __init__(self, action_space_shape):
+        self.action_space_shape = action_space_shape
+
+    def update(self, game_state, valid_actions):
+        pass
+
+    def get_action(self, valid_actions):
+        print(valid_actions)
+        mask = create_multidimensional_mask(torch.tensor(valid_actions), self.action_space_shape)
+        print("Mask shape:", mask.shape)
+
+        # use the mask to filter the valid actions by
+        valid_action_space = action_space * mask
+        # count nonzero elements
+        print("Number of nonzero elements:", torch.count_nonzero(valid_action_space), "Number of 1s in the mask:", torch.sum(mask))
+
+        # softmax and choose the action
+        valid_action_space = torch.softmax(valid_action_space, dim=len(action_space_shape) - 1)
+
+        # Flatten tensor and get argmax
+        flat_index = torch.argmax(valid_action_space.flatten())
+        
+        # Convert flat index back to multi-dimensional indices
+        action = np.unravel_index(flat_index.item(), action_space_shape)
+        # this is a np arr of np.int64, i want to convert it to a list of ints
+        action = [int(i) for i in action]
+
+        print("Action: ", action)
+
+class Actor(nn.Module):
+    def __init__(self, action_space_shape):
+        super(Actor, self).__init__()
+        self.actor = nn.Linear(action_space_shape, 1)
+    
+    def forward(self, x):
+        return self.actor(x)
+
+class Critic(nn.Module):
+    def __init__(self, action_space_shape):
+        super(Critic, self).__init__()
+        self.critic = nn.Linear(action_space_shape, 1)
+
+    def forward(self, x):
+        return self.critic(x)
+
 def create_multidimensional_mask(coordinates, shape):
-    """
+        """
     Create a multi-dimensional mask with zeros and set specified coordinates to 1.
 
     Parameters:
