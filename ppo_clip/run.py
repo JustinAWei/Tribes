7103037e5092bb8da94fb16e00b8fdbbbf9258e8
Justin Wei <wei.justin.a@gmail.com>
2024-12-14 15:26:50 -0800

    track game actions distributions

--- ppo_clip/run.py
+++ ppo_clip/run.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, Request
 import uvicorn
 import json
-
-from utils import filter_actions, action_tuples, index_to_action_tuple
+import time
+from utils import filter_actions, action_tuples, index_to_action_tuple, ACTION_TYPES
 from model import PPOClipAgent
 from utils import BOARD_LEN
-
+from pprint import pprint
 game_state_shape = (BOARD_LEN, BOARD_LEN, 27)
 action_space_shape = (BOARD_LEN, BOARD_LEN, len(action_tuples))
 
@@ -16,17 +16,45 @@ agent = PPOClipAgent(game_state_shape, action_space_shape)
 # Create FastAPI app
 app = FastAPI()
 
+current_game_id = 0
+current_tick = -1
+action_type_stats = {}
+
 @app.post("/receive")
 async def receive_data(request: Request):
     """
     Process and filter city actions from game state
     """
+    global current_game_id
+    global current_tick
+
     # Extract request data
     data = await request.json()
+
+    print(".", end="", flush=True)
 
     try:
         # Parse game state
         gs = json.loads(data['gameState']) if isinstance(data['gameState'], str) else data['gameState']
+
+
+        # if tick is 0, give a new current_game_id
+        if gs['tick'] == 0 and current_tick != 0:
+            if current_game_id in action_type_stats:
+                print("Game Ended", current_game_id)
+                print("Action Type Stats: ", action_type_stats[current_game_id])
+                # ACTION_TYPES is a dictionary of text to index, i want to print the text corresponding to the stats
+                for action_type, index in ACTION_TYPES.items():
+                    print(f"{action_type}: {action_type_stats[current_game_id].get(index, 0)}")
+
+            # timestamp
+            current_game_id = current_game_id + 1
+            action_type_stats[current_game_id] = {}
+
+            print("New game started", current_game_id)
+
+        current_tick = gs['tick']
+
         # pprint(gs)
 
         # BOARD_LEN = len(gs['board']['terrains'])
@@ -43,7 +71,9 @@ async def receive_data(request: Request):
         # print("Extra Var: ", extra_var)
 
         unpacked_action = [action[0], action[1], x2, y2, action_type, extra_var]
-        print("Unpacked Action: ", unpacked_action)
+        # print("Unpacked Action: ", unpacked_action)
+
+        action_type_stats[current_game_id][action_type] = action_type_stats[current_game_id].get(action_type, 0) + 1
 
         return {
             "status": 200, 
